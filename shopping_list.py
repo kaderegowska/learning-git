48602e6d07e9bca54bc902f5891ef3368f56e5e6
Katarzyna Deręgowska <ka.deregowska@gmail.com>
2022-08-17 20:49:30 +0200

    Paste shopping_list

---
+++ shopping_list.py
@@ -0,0 +1,14 @@
+shopping = {
+    "piekarnia" : ["chleb", "bułki", "pączek"],
+    "warzywniak" : ["marchew", "seler", "rukola"]
+}
+list1 = shopping['piekarnia']
+list2 = shopping['warzywniak']
+amount = len(list1) + len(list2)
+for i in range(len(list1)):
+    list1[i] = list1[i].capitalize()
+for j in range(len(list2)):
+    list2[j] = list2[j].capitalize()
+for sklep, produkty in shopping.items():
+    print(f"Idę do {sklep.title()} i kupuję tam {produkty}")
+print(f"W sumie kupuję {amount} produktów")
